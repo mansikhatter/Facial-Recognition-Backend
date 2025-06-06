bc4661787de98b74ec9e0f7d4b33ed3abc881f89
Mansi Khatter <157673034+mansikhatter@users.noreply.github.com>
2025-06-07 01:16:26 +0530

    Delete backup directory

--- backup/arg_parser.py
+++
@@ -1,8 +0,0 @@
-import argparse
-
-parser = argparse.ArgumentParser(description = 'Recognize Face Image')
-parser.add_argument('-indir', type=str, help='Input dir for Image')
-parser.add_argument('-outdir',type=str, help='Output dir for Name')
-
-args=parser.parse_args()
-print(args.indir)
